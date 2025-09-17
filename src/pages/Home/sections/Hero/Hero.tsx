50a1b4fcdb157df135373395b64839c9ffa45743
lucasmebo <lucasmebo@hotmail.com>
2025-09-16 21:14:17 -0300

    feat: Break point, estilizando e criar um theme

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -3,6 +3,8 @@ import Avatar from "../../../../assets/images/avatar.jpg"
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
+import DownloadIcon from '@mui/icons-material/ArrowCircleDown';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Hero = () => {
     const StyledHero = styled("div")(()=> ({
@@ -18,18 +20,28 @@ const Hero = () => {
   return (
     <>
      <StyledHero>
-        <Container>
-        <Grid container spacing={2}>
-          <Grid item size={4}>
-            <StyledImg src={Avatar} />
+        <Container maxWidth="lg">
+          <Grid container spacing={2}>
+            <Grid item size={{xs:12, md:4}}>
+              <StyledImg src={Avatar} />
+            </Grid>
+            <Grid item size={{xs:12, md:8}}>
+              <Typography color='primary' variant="h1" textAlign="center">Olá, sou o Lucas Borges</Typography>
+              <Typography color='primary' variant="h4" textAlign="center">Sou Analista de atualização e Implementação Jr.</Typography>
+              <Grid container display="flex" justifyContent="center">
+                <Grid item size={{xs:12, md:4}} display="flex" justifyContent="center">
+                  <Button>
+                    <DownloadIcon />
+                    Download CV</Button>
+                </Grid>
+                <Grid item size={{xs:12, md:4}} display="flex" justifyContent="center">
+                  <Button>
+                    <EmailIcon />
+                    Contato</Button>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item size={8}>
-            <Typography color='primary' variant="h1">Olá, eu sou Lucas Borges</Typography>
-            <Typography color='primary' variant="h3">Sou Analista de atualização e Implementação de Sistemas Jr.</Typography>
-            <Button>Download CV</Button>
-            <Button>Contato</Button>
-          </Grid>
-        </Grid>
         </Container>
      </StyledHero>
     </>
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#232323',
+    },
+    secondary: {
+      main: '#4f8e3e',
+    },
+  },
+});
+
+export default theme;
